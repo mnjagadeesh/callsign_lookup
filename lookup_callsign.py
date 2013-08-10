5ad957bf0cbf009a89a2d62f85b15a2671672fed
Jagadeesh Malakannavar <mnjagadeesh@gmail.com>
2013-08-10 09:02:37 +0530

    cleaned up

--- lookup_callsign.py
+++
@@ -1,6 +0,0 @@
-import qrz
-import ipdb; ipdb.set_trace()
-qrz = qrz.QRZ('./settings.cfg')
-result = qrz.callsign("w7atc")
-print result['fname'], result['name']
-print result['addr2'], result['state']
